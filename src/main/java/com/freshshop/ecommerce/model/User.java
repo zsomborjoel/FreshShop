e0f3045c72e6b6ab446916e453b6e5b80f9ec78b
zsomborjoel <zsomborjoel@gmail.com>
2020-01-21 08:41:14 +0100

    customer model

---
+++ src/main/java/com/freshshop/ecommerce/model/User.java
@@ -0,0 +1,150 @@
+package com.freshshop.ecommerce.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users", schema="freshshop")
+class User {
+
+    @Id
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "active")
+    private boolean active = true;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public User() {
+    }
+
+    public User(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
+    public boolean getActive() {
+        return this.active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public User id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public User email(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public User firstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public User lastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public User active(boolean active) {
+        this.active = active;
+        return this;
+    }
+
+    
+    public LocalDateTime getUpdatedAt() {
+        return this.updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(firstName, user.firstName) && Objects.equals(lastName, user.lastName) && active == user.active;
+        
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, firstName, lastName, active);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", email='" + getEmail() + "'" +
+            ", firstName='" + getFirstName() + "'" +
+            ", lastName='" + getLastName() + "'" +
+            ", active='" + isActive() + "'" +
+            "}";
+    }
+
+    
+}
--- src/main/resources/db/V1__Schema.sql
+++ src/main/resources/db/V1__Schema.sql
@@ -39,26 +39,26 @@ create schema freshshop;
 
 -- get_schema_create
 create table freshshop.users (
-   id          serial                   not null     ,
-   email       varchar(255)             not null     ,
-   first_name  varchar(255)             not null     ,
-   last_name   varchar(255)             not null     ,
+   id          serial                   not null ,
+   email       varchar(255)             not null ,
+   first_name  varchar(255)             not null ,
+   last_name   varchar(255)             not null ,
    active      bool                      default true,
-   inserted_at timestamp with time zone not null     ,
+   inserted_at timestamp with time zone default current_timestamp ,
    updated_at  timestamp with time zone not null     ,
    constraint pk_users primary key (id)
 )   ;
 create table freshshop.roles (
    id          serial                   not null,
    name        varchar(255)             not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp ,
    updated_at  timestamp with time zone not null,
    constraint pk_roles primary key (id)
 )   ;
 create table freshshop.user_roles (
    user_id     integer                  not null,
    role_id     integer                  not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp ,
    updated_at  timestamp with time zone not null,
    constraint pk_user_roles primary key (user_id,role_id)
 )   ;
@@ -66,7 +66,7 @@ create table freshshop.categories (
    id          serial                   not null,
    name        varchar(255)             not null,
    parent_id   integer                          ,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp ,
    updated_at  timestamp with time zone not null,
    constraint pk_categories primary key (id)
 )   ;
@@ -80,14 +80,14 @@ create table freshshop.products (
    discount_price    numeric                   default 0    ,
    quantity          integer                   default 0    ,
    taxable           bool                      default false,
-   inserted_at       timestamp with time zone not null      ,
+   inserted_at       timestamp with time zone default current_timestamp       ,
    updated_at        timestamp with time zone not null      ,
    constraint pk_products primary key (id)
 )   ;
 create table freshshop.tags (
    id          serial                   not null,
    name        varchar(255)             not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp ,
    updated_at  timestamp with time zone not null,
    constraint pk_tags primary key (id)
 )   ;
@@ -98,7 +98,7 @@ create table freshshop.sales_orders (
    coupon_id   integer                          ,
    session_id  varchar(255)             not null,
    user_id     integer                  not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp ,
    updated_at  timestamp with time zone not null,
    constraint pk_sales_orders primary key (id)
 )   ;
@@ -111,14 +111,14 @@ create table freshshop.coupons (
    multiple    bool                      default false,
    start_date  timestamp with time zone               ,
    end_date    timestamp with time zone               ,
-   inserted_at timestamp with time zone not null      ,
+   inserted_at timestamp with time zone default current_timestamp,
    updated_at  timestamp with time zone not null      ,
    constraint pk_coupons primary key (id)
 )   ;
 create table freshshop.product_tags (
    product_id  integer                  not null,
    tag_id      integer                  not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp,
    updated_at  timestamp with time zone not null,
    constraint pk_product_tags primary key (product_id,tag_id)
 )   ;
@@ -133,28 +133,28 @@ create table freshshop.cc_transactions (
    cc_num             varchar(255)                     ,
    cc_type            varchar(255)                     ,
    response           text                             ,
-   inserted_at        timestamp with time zone not null,
+   inserted_at        timestamp with time zone default current_timestamp,
    updated_at         timestamp with time zone not null,
    constraint pk_cc_transactions primary key (id)
 )   ;
 create table freshshop.sessions (
    id          varchar(255)             not null,
    data        text                             ,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp,
    updated_at  timestamp with time zone not null,
    constraint pk_sessions primary key (id)
 )   ;
 create table freshshop.product_statuses (
    id          serial                   not null,
    name        varchar(255)             not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp,
    updated_at  timestamp with time zone not null,
    constraint pk_product_statuses primary key (id)
 )   ;
 create table freshshop.product_categories (
    category_id integer                  not null,
    product_id  integer                  not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp,
    updated_at  timestamp with time zone not null,
    constraint pk_product_categories primary key (category_id,product_id)
 )   ;
@@ -167,7 +167,7 @@ create table freshshop.order_products (
    price       numeric                  not null,
    quantity    integer                  not null,
    subtotal    numeric                  not null,
-   inserted_at timestamp with time zone not null,
+   inserted_at timestamp with time zone default current_timestamp,
    updated_at  timestamp with time zone not null,
    constraint pk_order_products primary key (id)
 )   ;
