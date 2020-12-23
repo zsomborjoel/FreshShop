367c55a6806b82ba821f8ac5a78310bfe97cf781
zsomborjoel <zsomborjoel@gmail.com>
2020-12-23 12:20:21 +0100

    Add footer for products

--- freshshop-fe/src/components/Product.js
+++ freshshop-fe/src/components/Product.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export default class Product extends Component {
     render() {
-        const {img, inCart} = this.props.product;
+        const {title, img, price, inCart} = this.props.product;
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
@@ -17,6 +17,13 @@ export default class Product extends Component {
                                 (<p className="text-capitalize mb-0" disabled>in cart</p>) : 
                                 (<i className="fas fa-cart-plus" />)}
                         </button>
+                    </div>
+                    <div className="card-footer d-flex justify-content-between">
+                        <p className="align-self-center mb-0">{title}</p>
+                        <h5 className="text-blue font-italic mb-0">
+                            <span className="mr-1">$</span>
+                            {price}
+                        </h5>
                     </div>
                 </div>
             </ProductWrapper>
