77851deba2fb018dbd642baa2e94ec8d5b806c26
zsomborjoel <zsomborjoel@gmail.com>
2020-12-25 09:15:45 +0100

    Stlye product wrapper

--- freshshop-fe/src/components/Product.js
+++ freshshop-fe/src/components/Product.js
@@ -32,5 +32,28 @@ export default class Product extends Component {
 }
 
 const ProductWrapper = styled.div`
-
+    .card {
+        bolder-color: transparent;
+        transition: all 1s linear;
+    }
+    .card-footer {
+        background: transparent;
+        border-top:transparent;
+        transition: all 1s linear;
+    &:hover {
+        .card {
+            border: 0.04rem solid rgba(0, 0, 0, 0.2);
+            box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+        }
+        .card-footer {
+            background: rgba(247, 247, 247)
+        }
+    }
+    .img-container {
+        postition: relative;
+        overflow: hidden;
+    }
+    .img-container: hover .card-img-top {
+        transform: scale(1.2);
+    }
 `
